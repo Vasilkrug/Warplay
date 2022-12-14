901809843104bb4c4a1d1689ec761aa7d5e95855
vasilkrug <vkruglovec@gmail.com>
2022-12-14 20:54:21 +0300

    add Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <div className="logo_img">
+                <img src={require('../../assets/img/icons/blue-logo.png')} alt=""/>
+            </div>
+            <div className="logo_text">
+                <span>WARPLAY.CLOUD</span>
+            </div>
+        </div>
+    );
+};
+
+export default Logo;
