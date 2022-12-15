926b9db87ec38ea1715da1e7242c3bfe68ce444f
vasilkrug <vkruglovec@gmail.com>
2022-12-15 21:33:29 +0300

    add drop-down menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,18 @@ import Logo from "../Logo/Logo";
 import HeaderNav from "../HeaderNav/HeaderNav";
 
 const Header = () => {
+    const burgerMenuToggle = () => {
+    const burgerBtn = document.querySelector('.burger-btn');
+    const nav = document.querySelector('.header_nav')
+        burgerBtn.classList.toggle('burger-btn-active')
+        nav.classList.toggle('header_nav_active')
+    }
     return (
         <header>
             <section className="header_section">
                 <Logo/>
                 <HeaderNav/>
+                <a onClick={burgerMenuToggle} className="burger-btn" href="#"><span></span></a>
             </section>
         </header>
     );
--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -4,15 +4,13 @@ import './HeaderNav.css';
 const HeaderNav = () => {
     return (
         <nav className="header_nav">
-            <ul className="header_nav_list">
-                <li><a href="">Главная</a></li>
-                <li><a href="">Помощь</a></li>
-                <li><a href="">Скачать</a></li>
-                <li><a href="">Открыть в браузере</a></li>
+                <a href="">Главная</a>
+                <a href="">Каталог игр</a>
+                <a href="">Скачать</a>
+                <a href="">Открыть в браузере</a>
                 <li><a href="">Цены</a></li>
-                <li><a href="">Тех поддержка</a></li>
-                <li><a href="">Контакты</a></li>
-            </ul>
+                <a href="">Контакты</a>
+                <a href="">Войти</a>
         </nav>
     );
 };
