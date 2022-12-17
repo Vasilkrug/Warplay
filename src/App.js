898b79916231fd75664524f3b8b85775828920de
vasilkrug <vkruglovec@gmail.com>
2022-12-17 11:56:06 +0300

    add component HowItWork

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React  from 'react';
 import Header from "./components/Header/Header";
 import Slider from "./components/Slider/Slider";
 import {sliderImages} from "./assets/img/slides/assets";
+import HowItWork from "./components/HowItWork/HowItWork";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <div>
             <Header/>
             <Slider sliderImages={sliderImages}/>
+            <HowItWork/>
         </div>
     )
 }
---
+++ src/components/HowItWork/HowItWork.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './HowItWork.css';
+import Link from "../../UI/Link/Link";
+import Title from "../../UI/Title/Title";
+
+const HowItWork = () => {
+    return (
+        <section className="how_it_work">
+            <div className="how_it_work_img">
+            </div>
+            <div className="how_it_work_desc">
+                <Title text="Облачный гейминг"/>
+                <h2>Как это работает</h2>
+                <p>Наши сервера запускают игру, и передают
+                    вам картинку, которой вы можете управлять
+                    в реальном времени!</p>
+                <p>Приложение  запустится на ПК с процессором
+                    от 1.5 GHz , от 1 Gb RAM и доступом в интернет
+                    от 15 мбит/с
+                    на всех операционых системах
+                    Windows 7, 8, 10 MacOS и Linux .</p>
+                <Link text='Загрузить'/>
+            </div>
+        </section>
+    );
+};
+
+export default HowItWork;
